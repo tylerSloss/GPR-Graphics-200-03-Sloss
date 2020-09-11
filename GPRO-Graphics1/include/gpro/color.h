6386df448be1145ab8e1a9526c69541b67e338c9
Tyler Sloss <tyler.sloss@mymail.champlain.edu>
2020-09-11 09:56:55 -0400

    Chapter 6.1

--- GPRO-Graphics1/include/gpro/color.h
+++ GPRO-Graphics1/include/gpro/color.h
@@ -20,18 +20,15 @@ void write_color(std::ostream& out, vec3 pixel_color) {
         << static_cast<int>(255.999 * pixel_color.z) << '\n';
 }
 
-bool hit_sphere(const vec3& center, double radius, const ray& r) {
-	vec3 oc(r.origin().x - center.x, r.origin().y - center.y, r.origin().z - center.z);
-	float a = dot(r.direction(), r.direction());
-	float b = 2.0 * dot(oc, r.direction());
-	float c = dot(oc, oc) - radius * radius;
-	float discriminant = b * b - 4 * a * c;
-	return (discriminant > 0);
-}
+
 
 vec3 ray_color(const ray& r) {
-	if (hit_sphere(vec3(0, 0, -1), 0.5, r))
-		return vec3(1, 0, 0);
+    float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(vec3(r.at(t).x, r.at(t).y,r.at(t).z - -1));
+        vec3 temp((N.x + 1) * 0.5, (N.y + 1) * 0.5, (N.z + 1) * 0.5);
+        return temp;
+    }
 	vec3 unit_direction = unit_vector(r.direction());
 	float t = 0.5f * (unit_direction.y + 1.0f);
 	vec3 temp(((1.0f - t) * 1.0f + t * 0.5f), ((1.0f - t) * 1.0f + t * 0.7f), ((1.0f - t) * 1.0f + t * 1.0f));
--- GPRO-Graphics1/include/gpro/utilities.h
+++ GPRO-Graphics1/include/gpro/utilities.h
@@ -7,11 +7,29 @@
 
 vec3 unit_vector(vec3 dir)
 {
-	float scale = sqrt((dir.x * dir.x) + (dir.y * dir.y) + (dir.z * dir.z));
+	double temp = pow(dir.x, 2);
+	temp += pow(dir.y, 2);
+	temp += pow(dir.z, 2);
+	float scale = sqrt(temp);
 	dir.x = dir.x / scale;
 	dir.y = dir.y / scale;
 	dir.z = dir.z / scale;
 	return dir;
+}
+
+bool hit_sphere(const vec3& center, double radius, const ray& r) {
+	vec3 oc(r.origin().x - center.x, r.origin().y - center.y, r.origin().z - center.z);
+	float a = dot(r.direction(), r.direction());
+	float b = 2.0 * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = b * b - 4 * a * c;
+	if (discriminant < 0) {
+		return -1.0;
+	}
+	else {
+		return (-b - sqrt(discriminant)) / (2.0 * a);
+	}
+	return (discriminant > 0);
 }
 
 #endif
